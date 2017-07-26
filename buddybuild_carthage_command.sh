1b55e7c9e7516b3737328a358c2af73b3e438930
Romain Pouclet <romain.pouclet@gmail.com>
2017-07-26 16:06:08 -0400

    Add rome download

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -2,9 +2,4 @@
 
 brew install blender/homebrew-tap/rome
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY
-aws configure set aws_secret_access_key $AWS_SECRET_KEY
-# Optionally, you can also set the region of your bucket:
-aws configure set default.region us-west-2
-
-which aws
+rome download
